9abe7701252b10a6c96c1b2c33e3df9769feef35
LeeYeJ <116490956+LeeYeJ@users.noreply.github.com>
2023-05-08 17:52:55 +0900

    Create tf07_Linear1.py

---
+++ tf07_Linear1.py
@@ -0,0 +1,39 @@
+# y = wx + b
+
+import tensorflow as tf
+tf.set_random_seed(337) # 랜던 씨드 고정
+
+#1. 데이터
+x = [1,2,3]
+y = [1,2,3]
+
+w = tf.Variable(111, dtype=tf.float32)
+b = tf.Variable(0, dtype=tf.float32)
+
+#2. 모델 구성
+# y = wx + b -> 사실은 쌤이 그징말 했어 
+# y = xw + b이다 행렬은 순서가 중요하기 때문에 값이 다르다. ( 데이터에 웨이트가 연산되는거지 웨이트가 먼저있고 데이터가 연산되는게 아니니까)
+
+# y를 찾는 부분
+hypothesis = x * w + b
+
+#3-1. 컴파일
+loss = tf.reduce_mean(tf.square(hypothesis - y)) # hypothesis - y 의 제곱의 평균 -> mse (loss지표)
+
+# 웨이트 빼기 고정된 러닝레이트와 로스를 웨이트로 편미분 해준값을 곱한다 -> 값이 계속 작아진다.
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01) # 경사하강법 방식으로 optimizer를 최적화 시켜준다.
+train = optimizer.minimize(loss) # 따라서 로스를 최저로 뽑음 
+# model.complie(loss='mse', optimizer = 'sgd') 이것과 같다?
+
+#3-2 훈련
+sess = tf.compat.v1.Session()
+sess.run(tf.global_variables_initializer()) # 전체 변수 초기화
+
+#4 모델의 훈련
+epochs = 2001
+for step in range(epochs):
+    sess.run(train) # 에포당 그래프가 한번에 모두 연산됨
+    if step %20 ==0:
+        print(step,sess.run(loss), sess.run(w), sess.run(b))
+
+sess.close()
